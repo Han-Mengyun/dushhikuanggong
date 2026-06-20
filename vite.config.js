95d22968e6225a5e5612d0ee67bf6a5437c440c6
yuntian <1765705480@qq.com>
2026-06-20 16:54:22 +0800

    feat: 添加GitHub Pages自动部署配置

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  base: './',
+  base: '/dushhikuanggong/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
